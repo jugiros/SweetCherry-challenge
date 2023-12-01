abbf27948c83018be7c10f25a80f69cb30dd3218
jugiros <jugiros1@gmail.com>
2023-12-01 10:38:50 -0500

    fix: Query fix.

--- back-end/challenge/src/main/java/com/SweetCherry/challenge/repositories/ProductRepository.java
+++ back-end/challenge/src/main/java/com/SweetCherry/challenge/repositories/ProductRepository.java
@@ -10,6 +10,6 @@ import java.math.BigDecimal;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    @Query(value = "SELECT * FROM products p WHERE p.precio = :parametro OR p.titulo LIKE %:parametro%", nativeQuery = true)
+    @Query(value = "SELECT * FROM product p WHERE p.precio = :parametro OR p.titulo LIKE %:parametro%", nativeQuery = true)
     Page<Product> findByPrecioOrTitulo(String parametro, Pageable pageable);
 }
